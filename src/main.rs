4fd1a9bd53bb73cbad4ebf1d0ec5112effc702ff
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-05-06 11:17:29 +0400

    Listing 18-21: Modifications to main to use the new implementation of the blog post workflow

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
 
     post.add_text("I ate a salad for lunch today");
 
-    post.request_review();
+    let post = post.request_review();
 
-    post.approve();
+    let post = post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
 }
